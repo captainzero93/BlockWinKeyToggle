3664ce1caaaea028821bc6eafbc03265b9480a32
Joe Faulkner <cyberjunk77@protonmail.com>
2024-11-12 10:07:57 +0000

    Init

---
+++ BlockWinKey/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BlockWinKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toggleCheckBox = new System.Windows.Forms.CheckBox();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // toggleCheckBox
+            // 
+            this.toggleCheckBox.AutoSize = true;
+            this.toggleCheckBox.Location = new System.Drawing.Point(10, 10);
+            this.toggleCheckBox.Name = "toggleCheckBox";
+            this.toggleCheckBox.Size = new System.Drawing.Size(121, 17);
+            this.toggleCheckBox.TabIndex = 0;
+            this.toggleCheckBox.Text = "Block Windows Key";
+            this.toggleCheckBox.UseVisualStyleBackColor = true;
+            this.toggleCheckBox.CheckedChanged += new System.EventHandler(this.toggleCheckBox_CheckedChanged);
+            // 
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(10, 57);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(223, 131);
+            this.logTextBox.TabIndex = 2;
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Text = "Windows Key Blocker";
+            this.trayIcon.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 198);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.toggleCheckBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Key Blocker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox toggleCheckBox;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+    }
+}
---
+++ BlockWinKey/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Text;
+using System.Linq;
+
+namespace BlockWinKey
+{
+    public partial class Form1 : Form
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int VK_LWIN = 0x5B;
+        private const int VK_RWIN = 0x5C;
+        private const int MAX_LOG_LINES = 100;
+
+        private readonly LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+        private volatile bool _isWindowsKeyBlocked;
+        private readonly StringBuilder _logBuilder;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public Form1()
+        {
+            _logBuilder = new StringBuilder(128);
+            InitializeComponent();
+            _proc = HookCallback;
+            InitializeHook();
+            InitializeTrayIcon();
+        }
+
+        private void InitializeHook()
+        {
+            _hookID = SetHook(_proc);
+            Log("Hook initialized");
+        }
+
+        private void InitializeTrayIcon()
+        {
+            trayIcon.Icon = System.Drawing.SystemIcons.Application;
+            trayIcon.Visible = true;
+            trayIcon.Text = "Windows Key Blocker";
+
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Show", null, (s, e) => { Show(); WindowState = FormWindowState.Normal; });
+            contextMenu.Items.Add("Exit", null, Exit);
+
+            trayIcon.ContextMenuStrip = contextMenu;
+            trayIcon.DoubleClick += (s, e) => { Show(); WindowState = FormWindowState.Normal; };
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            Process curProcess = Process.GetCurrentProcess();
+            ProcessModule curModule = curProcess.MainModule;
+            try
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+            finally
+            {
+                if (curProcess != null) curProcess.Dispose();
+                if (curModule != null) curModule.Dispose();
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && _isWindowsKeyBlocked &&
+                (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == VK_LWIN || vkCode == VK_RWIN)
+                {
+                    Log($"Windows key blocked");
+                    return (IntPtr)1;
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private void toggleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            _isWindowsKeyBlocked = toggleCheckBox.Checked;
+            trayIcon.Text = $"Windows Key Blocker ({(_isWindowsKeyBlocked ? "ON" : "OFF")})";
+            Log($"Windows key blocking {(_isWindowsKeyBlocked ? "enabled" : "disabled")}");
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void Log(string message)
+        {
+            if (logTextBox.IsDisposed) return;
+
+            _logBuilder.Clear()
+                      .Append('[')
+                      .Append(DateTime.Now.ToString("HH:mm:ss"))
+                      .Append("] ")
+                      .Append(message);
+
+            if (logTextBox.InvokeRequired)
+            {
+                logTextBox.BeginInvoke(new Action(() => UpdateLog(_logBuilder.ToString())));
+            }
+            else
+            {
+                UpdateLog(_logBuilder.ToString());
+            }
+        }
+
+        private void UpdateLog(string message)
+        {
+            logTextBox.AppendText(message + Environment.NewLine);
+
+            var lines = logTextBox.Lines;
+            if (lines.Length > MAX_LOG_LINES)
+            {
+                logTextBox.Lines = lines.Skip(lines.Length - MAX_LOG_LINES).ToArray();
+            }
+
+            logTextBox.SelectionStart = logTextBox.TextLength;
+            logTextBox.ScrollToCaret();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+    }
+}
